b904a6fe503bc58a77f241c8f7982a94c0089964
EGGLAS <erikssonnicklas@hotmail.com>
2020-09-18 08:31:55 +0200

    New script to change Hewlett-Packard to HP.
    Change name on orginal folder and uploaded a new script that change all manufactures from Hewlett-Packard to HP to support the new name standard from Driver Automation Tool 6.4.9.

---
+++ Customizations for Modern Driver Management/ChangeMDMManufacturerCMPackage.ps1
@@ -0,0 +1,46 @@
+# Name: Nicklas Eriksson, IT-center
+# 
+# Purpose: Change all Moder Driver mangement package that has the name Hewlett-Packard in manufacturer field to HP.
+# Version: 1.0 - 2020-09-18
+# History: 1.0 - 2020-09-18: Script was created.
+
+# Variabels 
+$SiteCode = "CM1" # Site code 
+$ProviderMachineName = "LKSRVSCCM03.res.ludvika.intra" # SMS Provider machine name
+$DriverPackageName = "Drivers - HP*" # Control which package you want to change.
+$Manufacturer = "HP" # 
+
+# Customizations
+$initParams = @{}
+#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
+#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+# Do not change anything below this line
+
+# Import the ConfigurationManager.psd1 module 
+if((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+}
+
+# Connect to the site's drive if it is not already present
+if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SiteCode):\" @initParams
+
+$AllRetiredDriverPackage = Get-CMPackage -Name $DriverPackageName -fast #| Select-Object -Property Name, pkgSourcePath
+
+if ($AllRetiredDriverPackage.Manufacturer -eq "Hewlett-Packard")
+{
+    write-host "Starting to change Package manufacturer to HP from Hewlett-Packard" -ForegroundColor Yellow
+    foreach ($HP in $AllRetiredDriverPackage)
+    {
+    Set-CMPackage -Name $HP.Name -Manufacturer "$Manufacturer" -verbose
+    }
+}
+else
+{
+    Write-host "No Package found to change" -ForegroundColor Green
+}
