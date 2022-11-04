840e217bc12f6136ebcad2fbff68ff260dd513ce
Nicklas Eriksson <erikssonnicklas@hotmail.com>
2022-11-04 08:47:45 +0100

    Update AddComptuersToCollection.ps1

--- AddComptuersToCollection.ps1
+++ AddComptuersToCollection.ps1
@@ -6,6 +6,7 @@
  Current Version: 1.0
  Changelog: 1.0 - 2022-10-19 - Nicklas Eriksson -  Script was created.
             1.1 - 2022-10-10 - NicklaS Eriksson - Added support for single computer and collection name.
+            1.2 - 2022-11-04 - Nicklas Eriksson - Updated script to support multiple computers when adding single computers.
 
  How run the script:
   Create a text file that specify the path where the text file exists, adding multiple computers from text file to the collection by ID. 
@@ -13,9 +14,10 @@
  Enter a single computer to add to the collection by ID.
   .\AddComptuersToCollectionByID.ps1 -SiteCode LO2 -SiteServer siteserver.domain.local -CollectionID LO1000X3 -Computer "Test1000"
  Enter a single computername to add to the collection by Name
- .\AddComptuersToCollectionByID.ps1 -SiteCode LO2 -SiteServer siteserver.domain.local -CollectionName "Test computers IT" -ComputerName "Test1000"
+ .\AddComptuersToCollectionByID.ps1 -SiteCode LO2 -SiteServer siteserver.domain.local -CollectionName "Test computers IT" -ComputerName "Test1000","Test10001"
 
 #>
+
 
 [CmdletBinding(DefaultParameterSetName = "")]
 param(
@@ -30,16 +32,20 @@ param(
     [Parameter(Mandatory=$False, HelpMessage='Specify path to the csv-file that contains computer.')]
     [string]$ComputerFile,
     [Parameter(Mandatory=$False, HelpMessage='Enter computername.')]
-    [string]$ComputerName  
+    [string[]]$ComputerName  
 )
 
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
 if ([string]::IsNullOrEmpty($ComputerFile)) {
-    Write-host "Info: Fetching computers from file: $($ComputerFile)" -ForegroundColor Yellow
-    $Computers = Get-content -path $ComputerFile 
+        Write-host "Info: Text file is not specified, variable Computername was specified, will add $($ComputerName) to the collection" -Foregroundcolor Yellow
+    $Computers = $ComputerName
+
 }
 else {
-    Write-host "Info: Text file is not specified, variable Computername was specified, will add $($ComputerName) to the collection" -Foregroundcolor Yellow
-    $Computers = $ComputerName
+    Write-host "Info: Fetching computers from file: $($ComputerFile)" -ForegroundColor Yellow
+    $Computers = Get-content -path $ComputerFile 
+
 }
 
 # Site configuration
@@ -78,13 +84,14 @@ else {
     Write-host "Info: Fetching colletionid from collectionname: $($CollectionName)" -ForegroundColor Yellow
     $CollectionID = Get-CMCollection -Name $CollectionName -ErrorAction Stop  | Select-Object -ExpandProperty CollectionID
     
-    Write-host "$c"
     # Check if CollectionID contains something, tried with catch step but could not get that to work.
     if ([string]::IsNullOrEmpty($CollectionID))
     {
         Write-host "Error: Something went wrong, could not get collectionID from $($CollectionName)" -ForegroundColor Red
         Break
     }
+   
+    Write-host "Info:  - Found collectionid from Name: $CollectionID" -ForegroundColor Yellow
 }
 
 
@@ -93,7 +100,7 @@ foreach ($Computer in $Computers)
 {
     try {
         $ResourceID = Get-CMDevice -Name "$($Computer)" -fast -ErrorAction Stop | Select-object -ExpandProperty ResourceID
-        Write-host "Info: Adding $($Computer) to $CollectionID" -ForegroundColor Yellow
+        Write-host "Info: - Adding $($Computer) to $CollectionID" -ForegroundColor Yellow
         Add-CMDeviceCollectionDirectMembershipRule -CollectionId "$collectionID" -ResourceId $ResourceID
     
     }
@@ -105,14 +112,15 @@ foreach ($Computer in $Computers)
     }
 }
 
-Write-Host "Info: Updating collection" -ForegroundColor Yellow
+Write-Host "Info: Trigger update on the collection" -ForegroundColor Yellow
 $CollectionDate = Get-CMCollectionFullEvaluationStatus -id $collectionID | Select-Object -ExpandProperty LastRefreshTime
 Invoke-CMCollectionUpdate -CollectionId $collectionID
 
+Write-Host "Info: Checking every $($SleepTimer) second if collection has been refreshed" -ForegroundColor Yellow
 # Sleep until collection has beeen updated.
 do
 {
-    Write-host "Info: Checking every $($SleepTimer) second if collection has been refreshed, Collection $($collectionID) has not been refreshed yet" -ForegroundColor Yellow
+    Write-host " - Info: Collection $($collectionID) has not been refreshed yet" -ForegroundColor Yellow
     Start-Sleep $SleepTimer
     $CheckIfCollectionIsRefreshed = Get-CMCollectionFullEvaluationStatus -id $collectionID | Select-Object -ExpandProperty LastRefreshTime
     
@@ -122,4 +130,7 @@ until ($CollectionDate -ne $CheckIfCollectionIsRefreshed)
 Write-host "Info: Collection has been updated" -ForegroundColor Yellow
 Write-host "Info: Trigger machine policy on collectionID: $collectionID" -ForegroundColor Yellow
 Invoke-CMClientAction -CollectionId $collectionID -ActionType ClientNotificationRequestMachinePolicyNow
+
+Set-Location -Path $scriptPath
+
 Write-host "Info: Job is done" -ForegroundColor Green
