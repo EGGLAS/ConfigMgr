043e03ac1ec9703ca424abeacf6f99ff0f3c6601
Nicklas Eriksson <erikssonnicklas@hotmail.com>
2022-11-30 16:25:05 +0100

    Update Get-SoftwareUpatesFromSoftwareUpdateGroup.ps1

--- Windows Updates/Get-SoftwareUpatesFromSoftwareUpdateGroup.ps1
+++ Windows Updates/Get-SoftwareUpatesFromSoftwareUpdateGroup.ps1
@@ -1,12 +1,17 @@
 <#
  Name: Nicklas Eriksson
  Date: 2022-11-21
- Purpose: Getting software updates from Software Update group.
+ Purpose: Getting software updates from Software Update group. 
  Version: 1.0
  Changelog: 1.0 - 2022-11-21 - Nicklas Eriksson -  Script was created.
+            1.1 - 2022-11-21 - Nicklas Eriksson -  Updated the script with the paramter RunADR, it's now possible to run the ADRs again if some updates are missing. .
 
  How to run it:
+ This will get all updates from all Software update groups that starts with the name ADR: Windows Server.
  .\Get-SoftwareUpatesFromSoftwareUpdateGroup.ps1 -SiteCode LO2 -SiteServer siteserver.domain.local
+ This will get all updates from all Software update groups that starts with the name ADR: Windows Server and run the ADRs again if some update is missing.
+ .\Get-SoftwareUpatesFromSoftwareUpdateGroup.ps1 -SiteCode LO2 -SiteServer siteserver.domain.local -RunADR
+
 
 
 #>
@@ -15,7 +20,9 @@ param(
     [Parameter(Mandatory=$True, HelpMessage='Specify sitecode.')]
     [string]$SiteCode,
     [Parameter(Mandatory=$True, HelpMessage='Specify siteserver.')]
-    [string]$SiteServer
+    [string]$SiteServer,
+    [Parameter(Mandatory=$False, HelpMessage='Run Automatic Deployment Rules.')]
+    [switch]$RunADR
 )
 
 $SoftwareUpdateRule = "ADR: Windows Server*"
@@ -43,7 +50,7 @@ Set-Location "$($SiteCode):\" @initParams
 
 # Get Software update group based on variable $SoftwareUpdateRule
 try {
-    Write-host "Getting Software updates from Software Updategroup: $($SoftwareUpdateRule)"
+    Write-host "Getting all Software Update groups that starts with the following names: $($SoftwareUpdateRule)"
     $GetSoftwareUpdates = Get-CMSoftwareUpdateGroup | where-object LocalizedDisplayName -like $SoftwareUpdateRule  | Select-object *
     Write-host "Found: $($GetSoftwareUpdates.Count)"
     Write-host "The following Software Update groups were found"
@@ -76,5 +83,32 @@ else {
         Write-host "----------------------------------------------------------------------------------------" -ForegroundColor Yellow
     }
 }
+
+
+if ($RunADR)
+{
+    Write-host "Getting all ADRs that should be triggerd" -ForegroundColor Yellow
+    $GetAllADR = Get-CMSoftwareUpdateAutoDeploymentRule -fast | where-object Name -like $SoftwareUpdateRule  | Select-object Name
+    Write-host "The following Automatic Deployment Rules will be triggerd." -ForegroundColor Yellow
+       foreach ($ADR in $GetAllADR.Name)
+   {
+       Write-host " - Name: $($ADR)"
+   }
+
+    Write-host "The following Automatic Deployment Rules will be triggerd." -ForegroundColor Yellow
+
+    foreach ($ADR in $GetAllADR.Name)
+    {
+        Invoke-CMSoftwareUpdateAutoDeploymentRule -Name $ADR -WhatIf
+        Write-host " - $($ADR) was triggerd to run" -ForegroundColor Yellow
+    }
+}
+else
+{
+
+    Write-host "ADRs was not schedulde to be updated."
+
+}
+
 
 Set-Location -Path $scriptPath
